10ed0c96bc48c8af1d7f76bd525b2f825cfe7624
Luis <lugarbayo@gmail.com>
2025-10-21 23:49:53 +0200

    feature: first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+console.log("hello world");
+console.log("This is a simple Node.js application.");
+console.info("Application started successfully.");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+console.debug("Debugging information.");
+console.trace("Trace message for debugging.");
+console.log("Goodbye world!");
+console.log(globalThis); //variable global en toda nuestra aplicacion
+//apunta a window en el navegador
+//apunta a global en nodejs
+globalThis.console.log("This is a message from globalThis console.");
+*/
+
+//VAMOS A UTILIZAR EL PATRÓN DE DISEÑO : MÓDULO
+//COMMONJS
+
+const sum = require('./sum.js'); //importar la función sum desde el archivo sum.js
+console.log(sum(5,3));
+
---
+++ sum.js
@@ -0,0 +1,3 @@
+function sum (a,b) { return a + b;}
+
+module.exports = sum //exportar la función sum para que pueda ser utilizada en otros archivos
